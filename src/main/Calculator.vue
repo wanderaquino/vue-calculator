2960c685ceb1bd3cfa9bed2b4cb27406fc2d048c
Wander Aquino <wander.aquino@gmail.com>
2021-04-13 00:03:23 -0300

    Adicionando evento de escuta de click para log da operação escolhida na Calculator

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="calculator">
         <Display value="10000"/>
-        <Button label="AC" triple/>
-        <Button label="/" operation/>
-        <Button label="7"/>
-        <Button label="8"/>
-        <Button label="9"/>
-        <Button label="*" operation/>
-        <Button label="4"/>
-        <Button label="5"/>
-        <Button label="6"/>
-        <Button label="-" operation/>
-        <Button label="1"/>
-        <Button label="3"/>
-        <Button label="3"/>
-        <Button label="+" operation/>
-        <Button label="0" double/>
-        <Button label=","/>
-        <Button label="=" operation/>
+        <Button @onButtonClick="clearMemory" label="AC" triple/>
+        <Button @onButtonClick="setOperation" label="/" operation/>
+        <Button @onButtonClick="addDigit" label="7"/>
+        <Button @onButtonClick="addDigit" label="8"/>
+        <Button @onButtonClick="addDigit" label="9"/>
+        <Button @onButtonClick="setOperation" label="*" operation/>
+        <Button @onButtonClick="addDigit" label="4"/>
+        <Button @onButtonClick="addDigit" label="5"/>
+        <Button @onButtonClick="addDigit" label="6"/>
+        <Button @onButtonClick="setOperation" label="-" operation/>
+        <Button @onButtonClick="addDigit" label="1"/>
+        <Button @onButtonClick="addDigit" label="3"/>
+        <Button @onButtonClick="addDigit" label="3"/>
+        <Button @onButtonClick="setOperation" label="+" operation/>
+        <Button @onButtonClick="addDigit" label="0" double/>
+        <Button @onButtonClick="addDigit" label=","/>
+        <Button @onButtonClick="setOperation" label="=" operation/>
     </div>
 
 </template>
