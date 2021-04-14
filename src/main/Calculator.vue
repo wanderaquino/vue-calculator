08d24b64b38b6060314cb268e737307c56ccd3f6
Wander Aquino <wander.aquino@gmail.com>
2021-04-13 22:47:46 -0300

    Implementando a lógica da calculadora

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calculator">
-        <Display value="10000"/>
+        <Display :value="displayValue" />
         <Button @onButtonClick="clearMemory" label="AC" triple/>
         <Button @onButtonClick="setOperation" label="/" operation/>
         <Button @onButtonClick="addDigit" label="7"/>
@@ -16,7 +16,7 @@
         <Button @onButtonClick="addDigit" label="3"/>
         <Button @onButtonClick="setOperation" label="+" operation/>
         <Button @onButtonClick="addDigit" label="0" double/>
-        <Button @onButtonClick="addDigit" label=","/>
+        <Button @onButtonClick="addDigit" label="."/>
         <Button @onButtonClick="setOperation" label="=" operation/>
     </div>
 
@@ -27,17 +27,61 @@ import Display from "../components/Display";
 import Button from "../components/Button";
 
 export default {
+    data: function () {
+        return {
+            displayValue: "0",
+            clearedDisplay: false,
+            operation: null,
+            values: [0,0],
+            currentValue: "0"
+        }
+    },
     components: {Button, Display},
     methods: {
         clearMemory() {
-            console.log("Limpar Memória");
+            //Fazer o objeto retornar ao estado originao: target <- source
+            Object.assign(this.$data, this.$options.data());
         },
 
         setOperation (operation) {
+            if (this.currentValue === 0) {
+                this.operation = operation;
+                this.currentValue = 1;
+                this.clearedDisplay = true;
+            } else {
+                const equals = operation === "=";
+                const currentOperation = this.operation;
+
+                try {
+                    this.values[0] = eval(`${this.values[0]} ${currentOperation} ${this.values[1]}`);
+                }
+                catch (e) {
+                    this.$emit("onError", e);
+                }
+
+                this.values[1] = 0;
+                this.displayValue = this.values[0];
+                this.operation = equals ? null : operation;
+                this.currentValue = equals ? 0 : 1;
+                this.clearedDisplay = !equals;
+            }
             console.log(`Operation  ${operation}`);
         },
         addDigit(n) {
-            console.log("Add Digit "+ n);
+            if (n === "." && this.displayValue.includes(".")){return}
+
+            const clearedDisplay = this.displayValue === "0" || this.clearedDisplay;
+            const currentValue = clearedDisplay ? "" : this.displayValue;
+            const displayValue = currentValue + n;
+
+            this.displayValue = displayValue;
+            this.clearedDisplay = false;
+
+            if (n !== ".") {
+                const i = this.currentValue;
+                const newValue = parseFloat(displayValue);
+                this.values[i] = newValue;
+            }
         }
     }
 };
